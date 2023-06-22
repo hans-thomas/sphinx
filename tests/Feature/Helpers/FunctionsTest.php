a32b9621a97362de8e1199d6e33679e62d6c5761
mortazavi <s.mohammad027@live.com>
2023-06-22 15:07:00 +0330

    capture_sessionWithSeveralCalls test added;

--- tests/Feature/Helpers/FunctionsTest.php
+++ tests/Feature/Helpers/FunctionsTest.php
@@ -4,9 +4,11 @@
 
 	use Hans\Horus\Exceptions\HorusException;
 	use Hans\Sphinx\Exceptions\SphinxException;
+	use Hans\Sphinx\Helpers\Enums\SphinxCache;
 	use Hans\Sphinx\Models\Session;
 	use Hans\Sphinx\Tests\Factories\UserFactory;
 	use Hans\Sphinx\Tests\TestCase;
+	use Illuminate\Support\Facades\Cache;
 
 	class FunctionsTest extends TestCase {
 
@@ -27,6 +29,39 @@
 			self::assertEquals(
 				$user->withoutRelations()->toArray(),
 				$session->sessionable->toArray()
+			);
+		}
+
+		/**
+		 * @test
+		 *
+		 * @return void
+		 * @throws HorusException|SphinxException
+		 */
+		public function capture_sessionWithSeveralCalls(): void {
+			$user        = UserFactory::createNormalUser();
+			$deviceLimit = $user->getDeviceLimit();
+
+			// hit device limit
+			foreach ( range( 1, $deviceLimit ) as $counter ) {
+				$sessions[] = capture_session( $user );
+			}
+			// capture new session
+			$sessions[] = capture_session( $user );
+			// old session should be deleted from DB and Cache
+			self::assertCount(
+				2,
+				$user->sessions
+			);
+			$this->assertModelMissing( $sessions[ 0 ] );
+			self::assertFalse( Cache::has( SphinxCache::SESSION . $sessions[ 0 ]->id ) );
+			self::assertEquals(
+				$sessions[ 1 ]->withoutRelations()->toArray(),
+				$user->sessions[ 0 ]->toArray()
+			);
+			self::assertEquals(
+				$sessions[ 2 ]->withoutRelations()->toArray(),
+				$user->sessions[ 1 ]->toArray()
 			);
 		}
 
