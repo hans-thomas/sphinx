c30906ed6251946dcf3a54587a2e1d9391074038
mortazavi <s.mohammad027@live.com>
2023-06-24 10:46:04 +0330

    config file commented;

--- config/config.php
+++ config/config.php
@@ -3,8 +3,46 @@
 	use Illuminate\Database\Eloquent\Model;
 
 	return [
+
+		/*
+		|--------------------------------------------------------------------------
+		| Secret key
+		|--------------------------------------------------------------------------
+		|
+		| A static secret key that will use for encode and decode of the wrapper
+		| token.
+		|
+		*/
 		'secret'             => env( 'SPHINX_SECRET_KEY', '' ),
+
+		/*
+		|--------------------------------------------------------------------------
+		| Access token expiration
+		|--------------------------------------------------------------------------
+		|
+		| Determine the expiration time of the access token.
+		|
+		*/
 		'access_expired_at'  => '+1 hour',
+
+		/*
+		|--------------------------------------------------------------------------
+		| Refresh token expiration
+		|--------------------------------------------------------------------------
+		|
+		| Determine expiration time of the refresh token.
+		|
+		*/
 		'refresh_expired_at' => '+24 hour',
+
+		/*
+		|--------------------------------------------------------------------------
+		| Role model class
+		|--------------------------------------------------------------------------
+		|
+		| The respondent model for roles. related model must implement
+		| findAndCache method and also, keep up-to-date cached instances.
+		|
+		*/
 		'role_model'         => Model::class,
 	];
