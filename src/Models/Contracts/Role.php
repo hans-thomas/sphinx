258b19056e69a1fe876c72ede64f9b39d0328395
Mohammad Mortazavi <s.mohammad027@live.com>
2023-08-15 20:46:31 +0330

    role contract created for models;

---
+++ src/Models/Contracts/Role.php
@@ -0,0 +1,30 @@
+<?php
+
+	namespace Hans\Sphinx\Models\Contracts;
+
+	interface Role {
+
+		/**
+		 * Find the given id and cache the result
+		 *
+		 * @param int $id
+		 *
+		 * @return static
+		 */
+		public static function findAndCache( int $id ): self;
+
+		/**
+		 * Return version of the current instance
+		 *
+		 * @return int
+		 */
+		public function getVersion(): int;
+
+		/**
+		 * Increase the version by one unit
+		 *
+		 * @return bool
+		 */
+		public function increaseVersion(): bool;
+
+	}
